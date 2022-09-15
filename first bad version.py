d19bd0941e570a87c778b8ab892bda5433463137
ke-stack <kingkevo254@gmail.com>
2022-09-15 10:33:53 +0300

    solved first bad version challenge

---
+++ first bad version.py
@@ -0,0 +1,35 @@
+"""
+You are a product manager and currently leading a team to develop a new product. Unfortunately, the latest version of your product fails the quality check. Since each version is developed based on the previous version, all the versions after a bad version are also bad.
+
+Suppose you have n versions [1, 2, ..., n] and you want to find out the first bad one, which causes all the following ones to be bad.
+
+You are given an API bool isBadVersion(version) which returns whether version is bad. Implement a function to find the first bad version. You should minimize the number of calls to the API.
+"""
+
+# The isBadVersion API is already defined for you.
+# @param version, an integer
+# @return a bool
+# def isBadVersion(version):
+
+class Solution(object):
+    def firstBadVersion(self, n):
+        """
+        :type n: int
+        :rtype: int
+        """
+        
+        first = 1
+        last = n
+              
+        while first <= last:
+            midpoint = first + (last - first ) / 2
+            if isBadVersion(midpoint):
+                if not isBadVersion(midpoint-1):
+                    return midpoint
+                else:
+                    last = midpoint - 1
+            else:
+                first = midpoint + 1
+                
+        return -1
+    
