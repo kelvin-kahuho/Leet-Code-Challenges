b085618252a36ea7c82f4380a84074b08c034e4b
ke-stack <kingkevo254@gmail.com>
2022-09-16 09:19:47 +0300

    solved search insert position challenge

---
+++ Search Insert Position.py
@@ -0,0 +1,20 @@
+"""
+Given a sorted array of distinct integers and a target value, return the index if the target is found. If not, return the index where it would be if it were inserted in order.
+
+You must write an algorithm with O(log n) runtime complexity
+"""
+from bisect import insort
+class Solution:
+    def searchInsert(self, nums: List[int], target: int) -> int:
+        for number in nums:
+            if number == target:
+                index = nums.index(target)
+                return index
+            else:
+                if target not in nums:
+                    bisect.insort(nums, target) 
+                    index = nums.index(target)
+                    return index
+        return None
+                
+                
